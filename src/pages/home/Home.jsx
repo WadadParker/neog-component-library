b712a150a84eae6879acc22c8d4011bcbe5ce97b
Wadad Parker <wadadparker5@gmail.com>
2023-09-12 23:50:37 +0530

    feat: badge

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import styles from "./home.module.css";
 
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
-import {Alert, Avatar} from "../../components";
+import {Alert, Avatar, Badge} from "../../components";
 
 export const Home = () => {
   const imageLink="https://storage.ko-fi.com/cdn/useruploads/display/8a8b9790-7303-4aee-881a-23ab131fa400_gear5daycopy.png";
@@ -16,6 +19,12 @@ export const Home = () => {
         <Avatar size="sm" name="joyboy" src={imageLink}/>
         <Avatar size="lg" name="joyboy" src={imageLink}/>
         </section>
+      </section>
+      <section>
+      <Badge icon={<FontAwesomeIcon icon={faCartShopping} />} quantity={1} />
+      <Badge avatar={<Avatar size="sm" name="joyboy" src={imageLink}/>} status="online" />
+      <Badge avatar={<Avatar size="lg" name="joyboy" src={imageLink}/>} status="offline" />
+      <Badge avatar={<Avatar size="xxs" name="joyboy" src={imageLink}/>} status="busy" />
       </section>
     </div>
   )
