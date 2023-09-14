501de9374cc65b4c18c3d009e683ece0b6b1b60f
Wadad Parker <wadadparker5@gmail.com>
2023-09-14 18:34:07 +0530

    feat: home fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,20 @@
 import styles from "./home.module.css";
 
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../components";
 
 export const Home = () => {
-  const imageLink="https://storage.ko-fi.com/cdn/useruploads/display/8a8b9790-7303-4aee-881a-23ab131fa400_gear5daycopy.png";
+  const navigate = useNavigate();
   return (
     <div className={styles[`home-container`]}>
-      <h1>This is home for now, lets make alll them components</h1>
+      <h1>Wadad's Component Library</h1>
+      <h5>Welcome to my CSS component library made up of React components. Navigate to components
+        page and explore components you want to use in your project
+      </h5>
+      <span className={styles.button} onClick={()=>navigate("/components")}>
+      <Button text={"Get Started"} />
+      </span>
     </div>
   )
 }
